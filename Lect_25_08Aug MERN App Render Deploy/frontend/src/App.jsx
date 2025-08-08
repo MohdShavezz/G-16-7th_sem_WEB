9c5ab205a0393a6ade4040a1ff8fc340986cbac0
MohdShavezz <mohdpctebtech19cse@gmail.com>
2025-08-08 12:18:53 +0530

    mern full stack crud app deployed on render

---
+++ Lect_25_08Aug MERN App Render Deploy/frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import UserForm from './components/UserForm.jsx'
+const API = "/api/users"
+
+function App() {
+  
+  const [users, setUsers] = useState([])
+  const [editUser, setEditUser] = useState(null)
+
+  const fetchUsers = async () => {
+    const res = await axios.get(API)
+    setUsers(res.data)
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  async function handleDelete(userId) {
+    await axios.delete(`${API}/${userId}`)
+    fetchUsers()
+  }
+  async function handleSubmit(data) {
+    if (editUser) {
+      await axios.put(`${API}/${editUser._id}`, data)
+    } else {
+      await axios.post(API, data)
+    }
+    setEditUser(null)
+    fetchUsers()
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Crud App</h2>
+        <UserForm onSubmit={handleSubmit} initial={editUser} />
+        <ul>
+          {
+            users.map(user => (
+              <li key={user._id}>{user.name} {user.email}
+                <button onClick={() => setEditUser(user)}>Edit</button>
+                <button onClick={() => handleDelete(user._id)} >Delete</button>
+              </li>
+
+            ))
+          }
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
